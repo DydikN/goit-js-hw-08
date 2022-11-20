a3c14416160c3a5c36ea65ab8277a7d758bf7dce
DydikN <PingvinPain@gmail.com>
2022-11-20 20:58:33 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,42 @@
 import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form');
-const emailEl = document.querySelector('input[name="email"]');
-const messageEl = document.querySelector('textarea[name="message"]');
-const dataBase = {
-  email: '',
-  message: '',
-};
+const storage = 'feedback-form-state';
 
-formEl.addEventListener('input', throttle(textInput, 500));
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input');
+const messageEl = document.querySelector('textarea');
+const dataBase = {};
+
 formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(textInput, 500));
+
+savedText();
 
 function onFormSubmit(e) {
   e.preventDefault();
-  e.currentTarget.reset();
+  dataBase.email = formEl.elements.email.value;
+  dataBase.message = formEl.elements.message.value;
+
   console.log(dataBase);
+
+  formEl.reset();
+  localStorage.removeItem(storage);
 }
 
 function textInput(e) {
   dataBase[e.target.name] = e.target.value;
-  const stringifyData = JSON.stringify(dataBase);
-  localStorage.setItem('feedback-form-state', stringifyData);
+  localStorage.setItem(storage, JSON.stringify(dataBase));
 }
 
 function savedText() {
-  const getData = localStorage.getItem('feedback-form-state');
-  const parsedData = JSON.parse(getData);
-  if (parsedData) {
-    emailEl.value = parsedData.email;
-    messageEl.value = parsedData.message;
+  const savedForm = localStorage.getItem(storage);
+  if (savedForm) {
+    const parceSavedForm = JSON.parse(savedForm);
+    for (const prop in parceSavedForm) {
+      if (parceSavedForm.hasOwnProperty(prop)) {
+        formEl.elements[prop].value = parceSavedForm[prop];
+        dataBase[prop] = parceSavedForm[prop];
+      }
+    }
   }
 }
-
-savedText();
